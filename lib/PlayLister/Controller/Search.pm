25509b0de11a5e94fb5c9898e2498af9d096690b
Peter Black <sonicblend@gmail.com>
2015-10-17 10:31:50 +0100

    use Mojo::UserAgent to query deezer API for results

--- lib/PlayLister/Controller/Search.pm
+++ lib/PlayLister/Controller/Search.pm
@@ -4,30 +4,14 @@ use Mojo::Base 'Mojolicious::Controller';
 sub search_deezer {
     my $self = shift;
 
-    # an abbreviated deezer search result for:
-    # http://api.deezer.com/search/album?q=Aphex Twin - Syro
-    my $message = qq|{
-  "data": [
-    {
-      "id": "8664043",
-      "title": "Syro",
-      "link": "http://www.deezer.com/album/8664043",
-      "cover": "https://api.deezer.com/album/8664043/image",
-      "cover_small": "https://cdns-images.deezer.com/images/cover/078e414889e91347a098f9d370997f2c/56x56-000000-80-0-0.jpg",
-      "cover_medium": "https://cdns-images.deezer.com/images/cover/078e414889e91347a098f9d370997f2c/250x250-000000-80-0-0.jpg",
-      "cover_big": "https://cdns-images.deezer.com/images/cover/078e414889e91347a098f9d370997f2c/500x500-000000-80-0-0.jpg",
-      "genre_id": 106,
-      "nb_tracks": 12,
-      "record_type": "album",
-      "tracklist": "https://api.deezer.com/album/8664043/tracks",
-      "explicit_lyrics": false,
-      "type": "album"
-    }
-  ],
-  "total": 1
-}|;
+    my $q = $self->param('album');
+    # replace characters deezer doesn't like
+    $q =~ s/–/-/g;
 
-    $self->render(text => $message);
+    my $ua = Mojo::UserAgent->new;
+    my $message = $ua->get('http://api.deezer.com/search/album?q='.$q)->res->json;
+
+    $self->render(json => $message);
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -6,14 +6,15 @@ use Test::Mojo;
 my $t = Test::Mojo->new('PlayLister');
 $t->get_ok('/search/deezer/Aphex Twin – Syro')
   ->status_is(200)
-  ->content_like(qr|http://www.deezer.com/album/8664043|);
+  ->json_is('/total', 1) # expect a single result - if not, data may not match
+  ->json_has('/data')
+  ->json_has('/data/0')
+  ->json_has('/data/0/id')
+  ->json_is('/data/0/link', 'http://www.deezer.com/album/8664043');
 
-TODO: {
-    local $TODO = 'Deezer API call not yet implemented';
-
-    $t->get_ok('/search/deezer/Benjamin Clementine – At Least for Now')
-      ->status_is(200)
-      ->content_like(qr|http://www.deezer.com/album/9787118|);
-}
+$t->get_ok('/search/deezer/Benjamin Clementine – At Least for Now')
+  ->status_is(200)
+  ->json_is('/total', 1) # expect a single result - if not, data may not match
+  ->json_is('/data/0/link', 'http://www.deezer.com/album/9787118');
 
 done_testing();
