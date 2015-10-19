f0b6b2d297ff684fa8945e50fe99b5d3ef9d727a
Peter Black <sonicblend@gmail.com>
2015-10-19 10:28:53 +0100

    create a standard Release model (artist, title, link) and render this as json

---
+++ lib/PlayLister/Model/Release.pm
@@ -0,0 +1,22 @@
+package PlayLister::Model::Release;
+
+use Moose;
+use namespace::autoclean;
+
+has 'artist' => (is => 'ro', isa => 'Str', default => '');
+has 'title'  => (is => 'ro', isa => 'Str');
+has 'link'   => (is => 'ro', isa => 'Str');
+
+sub to_json {
+    my $self = shift;
+
+    return {
+        artist => $self->artist,
+        title  => $self->title,
+        link   => $self->link,
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
