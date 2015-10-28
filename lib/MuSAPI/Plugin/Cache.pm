5899f9b79db5b173e594aa325ef20532468de326
Peter Black <sonicblend@gmail.com>
2015-10-28 17:42:20 +0000

    better use of Mojo::IOLoop::Delay, to not have callbacks within IOLoop steps. remove moose, as im not comfortable with disabling the inline_constructor.

--- lib/MuSAPI/Plugin/Cache.pm
+++ lib/MuSAPI/Plugin/Cache.pm
@@ -1,26 +1,21 @@
 package MuSAPI::Plugin::Cache;
 use Mojo::Base 'Mojolicious::Plugin';
-
-use Moose;
 use Mojo::Redis2;
-use namespace::autoclean;
 
-has 'cache' => (is => 'rw', isa => 'Mojo::Redis2');
+# use the MOJO_REDIS_URL environment variable, or defaults to port :6379
+my $redis = Mojo::Redis2->new;
 
 sub register {
     my ($self, $app) = @_;
 
-    # use the MOJO_REDIS_URL environment variable, or defaults to port :6379
-    $self->cache(Mojo::Redis2->new());
-
     # helper deliberatly called cache and not redis, should the need
     # arise to swap it out for another key-value store
-    $app->helper(cache => sub { $self->redis(@_) });
+    $app->helper(cache => sub { $self->query_redis(@_) });
 
     return;
 }
 
-sub redis {
+sub query_redis {
     my ($self, $c, $query, $cb) = @_;
 
     unless ($query) {
@@ -31,17 +26,18 @@ sub redis {
     $c->delay(
         sub {
             my ($delay) = @_;
-#            warn "A. search redis for key: '$query'. steps remaining (", scalar @{$delay->remaining}, ")\n";
+#            warn "1. search redis. steps remaining (", scalar @{$delay->remaining}, ")\n";
 
-            $self->cache->get($query, $delay->begin);
+            # search redis
+            $redis->get($query, $delay->begin);
         },
         sub {
             my ($delay, $err, $message) = @_;
 
-            # found key in cache
+            # hit!
             if ($message) {
-                warn "cache success: '$query'\n";
-#                warn "B1. got a key from redis. steps remaining (", scalar @{$delay->remaining}, ")\n";
+                warn "cache hit for '$query'\n";
+#                warn "2. cache hit - return with cached message\n";
 
                 # fake a Mojo::Transaction::HTTP so the callback args match
                 # that of $self->ua->get()
@@ -52,33 +48,41 @@ sub redis {
                 return $cb->($c->ua, $tx);
             }
 
-            # key not previously cached, search for it
-#            warn "B2. no key found in redis - search. steps remaining (", scalar @{$delay->remaining}, ")\n";
-            $c->ua->get($query => sub {
-                my ($ua, $mojo) = @_;
+            # miss
+            warn "cache missed for '$query'\n";
+#            warn "2. cache missed. steps remaining (", scalar @{$delay->remaining}, ")\n";
+            $delay->pass;
+        },
+        sub {
+            my ($delay, $cb) = @_;
 
-                # save key-value in cache
-#                warn "B3. response fetched. steps remaining (", scalar @{$delay->remaining}, ")\n";
-                $c->delay(
-                    sub {
-                        my ($delay) = @_;
+            # search live
+#            warn "3. search. steps remaining (", scalar @{$delay->remaining}, ")\n";
+            $c->ua->get($query, $delay->begin);
+        },
+        sub {
+            my ($delay, $tx) = @_;
 
-#                        warn "C1. store response in redis. steps remaining (", scalar @{$delay->remaining}, ")\n";
-                        $self->cache->set($query => $mojo->res->body, $delay->begin);
-                    },
-                    sub {
-                        my ($delay, $err, $message) = @_;
+            # save key-value in redis
+#            warn "4. store response in redis. steps remaining (", scalar @{$delay->remaining}, ")\n";
+            $redis->set($query => $tx->res->body);
 
-#                        warn "C2. callback. steps remaining (", scalar @{$delay->remaining}, ")\n";
-                        return $cb->($ua, $mojo);
-                    }
-                );
-            });
+            $delay->pass($tx->res->body);
+        },
+        sub {
+            my ($delay, $message, $err) = @_;
+
+#            warn "5. return with message. steps remaining (", scalar @{$delay->remaining}, ")\n";
+
+            # fake a Mojo::Transaction::HTTP so the callback args match
+            # that of $self->ua->get()
+            my $tx = Mojo::Transaction::HTTP->new;
+            $tx->res->code(200);
+            $tx->res->body($message);
+
+            return $cb->($c->ua, $tx);
         },
     );
 }
-
-# Moose constructor disabled, as Mojolicious::Plugin has a constructor
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
 
 1;
